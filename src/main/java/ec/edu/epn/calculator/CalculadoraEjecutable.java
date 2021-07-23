0fe24c985272b39bed9a6cce0f9b786e32a569f1
juanp <juan.tovar@epn.edu.ec>
2021-07-22 23:25:39 -0500

    Cambio en el método main, se añade una segunda impresión de cada uno de los métodos con valores distintos

--- src/main/java/ec/edu/epn/calculator/CalculadoraEjecutable.java
+++ src/main/java/ec/edu/epn/calculator/CalculadoraEjecutable.java
@@ -13,6 +13,15 @@ public class CalculadoraEjecutable {
         System.out.println("c.multiplication(7,6) = "+ c.multiplication(7,6));
         //Usando el método de división
         System.out.println("c.division(42,6) = "+ c.division(42,6));
-
+        //
+        System.out.println("******Cambio en el main");
+        //Usando el método de suma
+        System.out.println("c.addition(5,10) = "+ c.addition(5,10));
+        //Usando el método de resta
+        System.out.println("c.subtraction(8,2) = "+ c.subtraction(8,2));
+        //Usando el método de multiplicación
+        System.out.println("c.multiplication(5,12) = "+ c.multiplication(5,12));
+        //Usando el método de división
+        System.out.println("c.division(6,2) = "+ c.division(6,2));
     }
 }
